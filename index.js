404498292a09420d22a64c21f4264728e02961be
Gaurav <gg60473@gmail.com>
2026-06-24 15:10:35 +0530

    add all project files

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+
+const app = express();
+const cors = require("cors");
+
+const employeeRoutes = require("./routes/employeeRoutes");
+
+const loggerMiddleware = require("./middleware/loggerMiddleware");
+
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.use(loggerMiddleware);
+
+
+// Routes
+
+app.use("/employees", employeeRoutes);
+
+
+app.get("/", (req, res) => {
+
+  res.send("Employee Management API Running");
+
+});
+
+
+app.listen(4000, () => {
+
+  console.log("Server Running on Port 4000");
+
+});
